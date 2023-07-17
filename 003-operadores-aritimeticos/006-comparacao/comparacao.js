48eb6e717e2843ee8d105ae1ac80146e93a5e747
zLey-Santos <wy.santos92@gmail.com>
2023-07-17 13:37:47 +0100

    operadores de comparação

--- 003-operadores-aritimeticos/006-comparacao/comparacao.js
+++ 003-operadores-aritimeticos/006-comparacao/comparacao.js
@@ -7,3 +7,7 @@ console.log("2 ", 1 === "1"); // false verifica o tipo
 console.log("3 ", 1 != "1"); // falso verifica o valor sendo 1 nao é diferente de '1 string '
 
 console.log("4 ", 1 !== "1"); // true verifica o valor eo tipo sendo 1 diferente de '1 string'
+
+/* 
+devemos sempre usar os operadores estritos para verifocar os valor
+*/
