eac30b9a5746672688c906726c90434bf9792d47
zLey-Santos <wy.santos92@gmail.com>
2023-07-17 15:34:29 +0100

    preced

---
+++ 003-operadores-aritimeticos/008-precedencia/precedencia.js
@@ -0,0 +1,6 @@
+// Ordem de precedência
+
+console.log((5 * 2) / 5 === 2); // true
+// Pela ordem de precedência resolvemos primeiro o que está entre parenteses (primeiro a ser resolvido ) depois o que está por fora da questao.
+
+console.log((50 / 4) * 10 === 120 && 2 * 2 === 4); // false pois 12.5 * 10 = 125 como apenas um lado corresponde ao valor true e outro o valor false o resultado é false
