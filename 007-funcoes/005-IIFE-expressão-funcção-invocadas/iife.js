d832f561405ccf54862648d41a6e01bbec64371c
zLey-Santos <wy.santos92@gmail.com>
2023-07-31 17:28:50 +0100

    IIFE

---
+++ 007-funcoes/005-IIFE-expressão-funcção-invocadas/iife.js
@@ -0,0 +1,7 @@
+// IIFE = Immediately Invokad Function Expression
+// IIFE = Expressão de função invocada imediatamente
+
+(function (num1, num2, num3) {
+  const calc = num1 + num2 - num3;
+  console.log(calc);
+})(1, 4, 3);
