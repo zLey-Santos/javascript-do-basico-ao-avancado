d966e4ad879de32cdd03ec51965044dd41085022
zLey-Santos <wy.santos92@gmail.com>
2023-08-01 20:08:06 +0100

    js plus console.log(exercicios)

---
+++ 000-plus-javascript/001-console.log/console.js
@@ -0,0 +1,3 @@
+console.log("Hellow");
+console.log("World");
+console.log("!");
---
+++ 000-plus-javascript/002-exercicios/console.js
@@ -0,0 +1,9 @@
+console.log(
+  `Meu nome é Wesley. Estou apredendo javascript às  ${19}Hs da tarde.`
+);
+
+console.log(
+  "Meu nome é Wesley. Estou apredendo javascript às ",
+  19,
+  "Hs da atrde."
+);
