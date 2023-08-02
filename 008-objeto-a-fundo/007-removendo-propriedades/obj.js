5c26763cc824cc1a07d7cac287d76bab2abb623f
zLey-Santos <wy.santos92@gmail.com>
2023-08-02 22:36:26 +0100

    deletando propriedades do obj...

---
+++ 008-objeto-a-fundo/007-removendo-propriedades/obj.js
@@ -0,0 +1,15 @@
+let carro = {
+  cv: 250,
+  estoque: true,
+};
+
+carro.cv = 350;
+carro.estoque = false;
+carro.preco = 75000;
+carro["preco"] = 80000;
+console.log(carro["preco"]);
+
+delete carro.cv;
+// podemos deletar valores de um obj...
+
+console.log(carro.cv, carro.estoque);
