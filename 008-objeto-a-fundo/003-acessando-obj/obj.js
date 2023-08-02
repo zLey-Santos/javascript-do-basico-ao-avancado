77bb4a703a7cf9f8aec11e8e5a023ba8472a4e28
zLey-Santos <wy.santos92@gmail.com>
2023-08-02 21:00:00 +0100

    destructuring

--- 008-objeto-a-fundo/003-acessando-obj/obj.js
+++ 008-objeto-a-fundo/003-acessando-obj/obj.js
@@ -1,8 +1,13 @@
+// Definindo um objeto chamado 'tenis'
 const tenis = {
+  // Definindo a propriedade 'tamanho' com o valor 45
   tamanho: 45,
+
+  // Definindo a propriedade 'marca' com o valor "Nike"
   marca: "Nike",
 };
 
+// Imprimindo o tipo de dado da propriedade 'marca'
 console.log(typeof tenis.marca);
 
-// Para acessar os valores usamos (.) objeto.propriedade = valor
+// Nota: Para acessar os valores das propriedades de um objeto, usa-se a notação de ponto, por exemplo: tenis.marca para acessar o valor "Nike".
