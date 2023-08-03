d077ecffdbdba9f2c3fecc790400fddf84e01957
zLey-Santos <wy.santos92@gmail.com>
2023-08-03 16:51:06 +0100

    spread operator

---
+++ 009-arrays-a-fundo/005-spread-operador/spreadoperation.js
@@ -0,0 +1,8 @@
+const num = [1, 2, 3, 4, 5];
+
+console.log(...num); // spread sao os ... usado para mostrar um valor de um array de forma masi limpa
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.max(num)); // NaN pois é um arrya e não um número
+console.log(Math.max(...num)); // podemos usar o spread ... para buscar o maio valor dentro do arrya
+
+// Math é usado para conceitos de matematica basica nesse caso mesclado com o Math.max busca o maior valor
