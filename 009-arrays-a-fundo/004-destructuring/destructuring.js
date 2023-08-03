9f5860bf4da8dadeb48397e8948b5471d7dacda5
zLey-Santos <wy.santos92@gmail.com>
2023-08-03 16:27:26 +0100

    Destructuring

---
+++ 009-arrays-a-fundo/004-destructuring/destructuring.js
@@ -0,0 +1,10 @@
+// Matrizes Multidimensionais
+
+const [a, b, c] = ["Arroz", "Feijão", "Bife"];
+const comida = ["Arroz", "Feijão", "Bife"];
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log(1, comida[0], 2, comida[1], 3, comida[2]);
