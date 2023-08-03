00317e36270c4017d006e98b5857a9921d54c4f8
zLey-Santos <wy.santos92@gmail.com>
2023-08-03 14:32:16 +0100

    Índices e elemntos de um array

---
+++ 009-arrays-a-fundo/002-indices-elementos/arr.js
@@ -0,0 +1,10 @@
+// Indice e elementos
+// Array sempre inicializa pelo seu indice 0, 1, 2, 3, 4, etc..
+
+const arr = [1, 2, 3, 4, 5, true, "zLey", { nome: "zLey_Santos" }];
+
+const arr1 = [1, 2, 3, 4, true, "zLey", "Santos"];
+
+console.table(arr1); // Mostra uma tabela o index(indice) e value(valores)
+console.log(arr1[6]); // Pega a possição ou seja o indice do nosso array | para resgatar um possição usamos [colchetes]
+console.log(arr1.length); // Exibe o tamanho do meu array quantos valores tem dentro do array
