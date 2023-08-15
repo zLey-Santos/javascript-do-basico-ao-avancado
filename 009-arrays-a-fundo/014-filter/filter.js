5a8784a85bbc28fa2bf04bd87d14dd42fcd2d898
zLey-Santos <wy.santos92@gmail.com>
2023-08-15 23:47:17 +0100

    filter

---
+++ 009-arrays-a-fundo/014-filter/filter.js
@@ -0,0 +1,38 @@
+const pedidos = [
+  {
+    id: 420,
+    nome: "Wesley ",
+    alimento: "sanduba de bacon",
+    bebida: "Suco de limão",
+  },
+  {
+    id: 152,
+    nome: "Luana ",
+    alimento: "sanduba vegano",
+    bebida: "Suco de laranja",
+  },
+  {
+    id: 29,
+    nome: "Adam ",
+    alimento: "Petisco de frango",
+    bebida: "Água",
+  },
+  {
+    id: 33,
+    nome: "zLey ",
+    alimento: "Sandubão completo",
+    bebida: "Refrigerante",
+  },
+  {
+    id: 55,
+    nome: "zé ",
+    alimento: "Pizza",
+    bebida: "Refrigerante",
+  },
+];
+
+const filterRefri = pedidos.filter((Element, index) => {
+  return Element.bebida === "Refrigerante";
+});
+
+console.log(filterRefri);
