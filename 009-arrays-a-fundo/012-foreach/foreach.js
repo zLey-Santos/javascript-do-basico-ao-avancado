ba4f3db5b3e71f987b8a42d7f11737ad03cd5f83
zLey-Santos <wy.santos92@gmail.com>
2023-08-15 22:49:52 +0100

    ´foreach´

---
+++ 009-arrays-a-fundo/012-foreach/foreach.js
@@ -0,0 +1,12 @@
+const myArrayObj = [
+  { nome: "Wesley ", sobreNome: "Santos" },
+  { nome: "Luana ", sobreNome: "Valentim" },
+  { nome: "Adam ", sobreNome: "VAlentim" },
+];
+
+myArrayObj.forEach((item, index) => {
+  if (item.nome === "Luana ") {
+    return console.log("Vamos ao veterinaio hoje sem marcação previa");
+  }
+  console.log(index, item.nome);
+});
