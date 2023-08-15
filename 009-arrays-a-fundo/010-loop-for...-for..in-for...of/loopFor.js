0f2c87b6b71315f75e5b80868d157f05a653ec4e
zLey-Santos <wy.santos92@gmail.com>
2023-08-15 15:00:47 +0100

    loop for..., for...of, for...in

---
+++ 009-arrays-a-fundo/010-loop-for...-for..in-for...of/loopFor.js
@@ -0,0 +1,33 @@
+const myArray = [1, 2, 3, 4, 5, 6];
+
+const myArrayObj = [
+  { nome: "Wesley ", sobreNome: "Santos" },
+  { nome: "Luana ", sobreNome: "Valentim" },
+  { nome: "Adam ", sobreNome: "VAlentim" },
+];
+
+//for([inicialização]; [condição]; [expreção final])
+
+// for...
+/* for (let i = 0; i <= 10; i++) {
+  console.log(i);
+} */
+
+/* for (let i = 0; i < myArrayObj.length; i++) {
+  console.log(myArrayObj[i].nome, myArrayObj[i].sobreNome);
+} */
+
+//for...of podemos trabalhar com arrays
+/* for (let item of myArrayObj) {
+  console.log(item.nome, item.sobreNome);
+}
+
+for (let item of myArray) {
+  console.log(item);
+} */
+
+//for...in usamos para trabalhar com objetos
+const obj = { nome: "Wesley", sobreNome: "Santos" };
+for (let item in obj) {
+  console.log(item);
+}
