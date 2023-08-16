efc586080a250c1985541d0aa10fb804438b04c6
zLey-Santos <wy.santos92@gmail.com>
2023-08-16 23:08:11 +0100

    getElementByTagName

--- 010-dom/005-getElementByClassName/dom.js
+++ 010-dom/005-getElementByClassName/dom.js
@@ -1,5 +1,6 @@
-const p = document.getElementsByClassName("paragrafo");
-console.log(p);
+const p = document.getElementsByTagName("p");
+
+// so funciona com a propria tag so html h1, p, strong etc...
 
 for (let element of p) {
   console.log(element);
