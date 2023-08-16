09128177ec9b423281bddffac55572854a3e7891
zLey-Santos <wy.santos92@gmail.com>
2023-08-16 19:18:43 +0100

    seleciondo elemento DOM

---
+++ 010-dom/003-selecionando-elemento/dom.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const paragrafos = document.querySelectorAll("p");
+
+paragrafos.forEach((element, index) => {
+  element.style.color = "#FFF";
+  element.style.padding = "12px";
+  element.style.borderRadius = "12px";
+
+  if (index === 0) {
+    element.style.background = "blue";
+  }
+  if (index === 1) {
+    element.style.background = "red";
+  }
+  console.log("indice ", index, "elemento ", element);
+});
