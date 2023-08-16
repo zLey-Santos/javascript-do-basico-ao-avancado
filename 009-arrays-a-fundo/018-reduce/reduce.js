7faf201d15ca0f9466c46648c3a74fbd46c488e8
zLey-Santos <wy.santos92@gmail.com>
2023-08-16 16:16:36 +0100

    reduce

--- 009-arrays-a-fundo/018-reduce/reduce.js
+++ 009-arrays-a-fundo/018-reduce/reduce.js
@@ -4,35 +4,40 @@ const pedidos = [
     nome: "Wesley ",
     alimento: "sanduba de bacon",
     bebida: "Suco de limão",
+    preco: 80,
   },
   {
     id: 152,
     nome: "Luana ",
     alimento: "sanduba vegano",
     bebida: "Suco de laranja",
+    preco: 50,
   },
   {
     id: 29,
     nome: "Adam ",
     alimento: "Petisco de frango",
     bebida: "Água",
+    preco: 35,
   },
   {
     id: 33,
     nome: "zLey ",
     alimento: "Sandubão completo",
     bebida: "Refrigerante",
+    preco: 100,
   },
   {
     id: 55,
     nome: "zé ",
     alimento: "Pizza",
     bebida: "Refrigerante",
+    preco: 85,
   },
 ];
 
-const existeAlimento = pedidos.some((Element, index) => {
-  return Element.alimento === "Pizza";
-});
+const balanco = pedidos.reduce((total, element) => {
+  return total + element.preco;
+}, 0);
 
-console.log(existeAlimento); // O Some pergunta se apenas 1 valor dos Array equivale a sua condição.
+console.log(`R$${balanco} `); // A ideia é pegar todos os valores de um array e condensá-los em um só
