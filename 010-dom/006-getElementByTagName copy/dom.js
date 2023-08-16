011252c26623a41381e9649ff588dc8a791689bb
zLey-Santos <wy.santos92@gmail.com>
2023-08-16 23:17:59 +0100

    DOM resumo

---
+++ 010-dom/006-getElementByTagName copy/dom.js
@@ -0,0 +1,12 @@
+/**
+ * Mais qual devo usar?
+ * ****************************************
+ * Gosto muito desses dois
+ * querySelector
+ * querySelectorAll  => Retona um NodeList
+ * ***************************************
+ * Pode usar porém não gosto ou uso bem POUCO
+ * getElementById
+ * getElementsByClassName
+ * getElementsByTagName
+ **/
