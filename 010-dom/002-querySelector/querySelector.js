7603a085edfdf593c4b5a493d9b3bbf5d8a4f873
zLey-Santos <wy.santos92@gmail.com>
2023-08-16 19:03:52 +0100

    manipulando dom querySelect - selecionado elemento

---
+++ 010-dom/002-querySelector/querySelector.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const h1 = document.querySelector("h1");
+h1.style.background = "red";
+
+const classP = document.querySelector(".paragrafo");
+classP.style.background = "blue";
+
+const idP = document.querySelector("#paragrafo");
+idP.style.background = "red";
+idP.style.color = "yellow";
+
+const strong = document.querySelector("strong");
+strong.style.background = "yellow";
+
+let timer = 0;
+const title = document.querySelector("title");
+
+setInterval(() => {
+  title.innerText = timer;
+  timer++;
+}, 1000000);
