a47ec8d28d3855125664a0e5aaf371c82c54f252
zLey-Santos <wy.santos92@gmail.com>
2023-08-17 01:13:40 +0100

    alterando atributos

--- 011-manipulando-valores/004-atributos/dom.js
+++ 011-manipulando-valores/004-atributos/dom.js
@@ -1,19 +1,10 @@
-/**
- * inner/html => REtorna o texto, com formatações e com elementos HTML\
- * createElement ==> Cria um elemento HTML
- **/
+"use strict";
+
 const div = document.querySelector("div");
-console.log(1, div);
+div.setAttribute("id", "teste");
 
-div.innerHTML = `${div.innerHTML}  <strong> Meu texto atualizado...</strong>`;
-console.log(2, div);
+div.classList.add("active");
+console.log(div);
+div.classList.add("background-red");
 
-const elementUl = document.createElement("ul");
-
-[1, 2, 3, 4, 5].forEach((element) => {
-  const elementLi = document.createElement("li");
-  elementLi.innerText = element;
-  elementUl.appendChild(elementLi);
-});
-
-div.appendChild(elementUl);
+console.log(div);
