4d34a75f67dc093f355d2d59e7cbd174957dcadb
zLey-Santos <wy.santos92@gmail.com>
2023-08-17 17:01:18 +0100

    manipulando atributos com DOM

---
+++ 011-manipulando-valores/004-atributos copy/dom.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const div = document.querySelector("div");
+div.setAttribute("id", "teste");
+
+div.classList.add("active");
+
+div.classList.add("background-red");
+div.removeAttribute("id");
+
+console.log(div);
