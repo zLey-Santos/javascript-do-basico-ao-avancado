c1639b46ab903219571114706a126bb0b9a92da2
zLey-Santos <wy.santos92@gmail.com>
2023-08-18 11:48:41 +0100

    manipulando style

---
+++ 012-manipulando-style-DOM/001-manipulando-style/dom.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector("h1");
+h1.style.fontFamily = "Arial ";
+h1.style.color = "red";
+
+const p = document.querySelectorAll("p");
+p.forEach((element) => {
+  element.style.fontFamily = "Arial";
+  element.style.fontSize = "12px";
+  element.style.backgroundColor = "gray";
+  element.style.borderRadius = "8px";
+  element.style.padding = "8px";
+});
