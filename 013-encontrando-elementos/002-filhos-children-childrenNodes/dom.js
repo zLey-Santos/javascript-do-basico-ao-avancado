5e9d62df612134650cd609f722062a136af5ee3d
zLey-Santos <wy.santos92@gmail.com>
2023-08-18 13:14:01 +0100

    children e childNodes

--- 013-encontrando-elementos/002-filhos-children-childrenNodes/dom.js
+++ 013-encontrando-elementos/002-filhos-children-childrenNodes/dom.js
@@ -1,16 +1,23 @@
+// Ativar o modo estrito para aplicar regras rigorosas de JavaScript
 "use strict";
 
+// Selecionar um elemento com a classe "container" usando querySelector
 const container = document.querySelector(".container");
 
+// Imprimir a coleção de elementos filhos do elemento "container"
 console.log(container.children); // HTMLCollection
+
+// Imprimir a lista de nós filhos do elemento "container"
 console.log(container.childNodes); // NodeList
 
-// console.log(children);
-
+// Imprimir uma linha de separação no console para melhorar a legibilidade
 console.log("--------------------------------");
 
-const children = Array.from(container.children); // trasformar os filhos em arr e iteralos
+// Transformar a coleção de elementos filhos em um array usando Array.from()
+const children = Array.from(container.children);
 
+// Iterar sobre cada elemento no array "children" usando forEach
 children.forEach((res) => {
+  // Imprimir cada elemento no console durante a iteração
   console.log(res);
 });
