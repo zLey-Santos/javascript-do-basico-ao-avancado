54c6708a7f05c11ca6a4df141d8a09561b769fd7
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 21:33:46 +0100

    use strict

--- 007-funcoes/006-use-strict/useStrict.js
+++ 007-funcoes/006-use-strict/useStrict.js
@@ -1,7 +1,7 @@
 /* 
 O strict mode elimina alguns erros silenciosos que passarian batidos no javascript e os emitir erros.
 
-Além disso, essa forma corrige alguns erros que tornam o javascript difícil
+Além disso, essa forma corrige alguns erros que tornam o javascript difícil..
 de ser otimizado, então algumsl vezes os códigos no modo estrito rodam masi otimozados e velozes do que os codigos no 'modo normal '
 link: www.geeksforgeeks,org/stric-mode-javascript/
 */
