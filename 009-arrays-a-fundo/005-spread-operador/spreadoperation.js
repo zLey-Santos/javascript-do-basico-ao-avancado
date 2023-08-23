d0311ef9b81a9a395f9aee518c31cc52ab67a9d8
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 21:51:57 +0100

    spread operation

--- 009-arrays-a-fundo/005-spread-operador/spreadoperation.js
+++ 009-arrays-a-fundo/005-spread-operador/spreadoperation.js
@@ -5,4 +5,4 @@ console.log(Math.max(1, 2, 3, 4, 5));
 console.log(Math.max(num)); // NaN pois é um arrya e não um número
 console.log(Math.max(...num)); // podemos usar o spread ... para buscar o maio valor dentro do arrya
 
-// Math é usado para conceitos de matematica basica nesse caso mesclado com o Math.max busca o maior valor
+// Math é usado para conceitos de matematica basica nesse caso mesclado com o Math.max busca o maior valor.
