afbf56d01699be08a65ff4b7ad0fcd0aaef686bf
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 22:07:48 +0100

    classList

--- 012-manipulando-style-DOM/002-classList/dom.js
+++ 012-manipulando-style-DOM/002-classList/dom.js
@@ -6,7 +6,7 @@ h1.classList.add("active", "teste1"); // add uma class
 h1.classList.add("teste2");
 
 h1.classList.remove("teste1"); // remove uma class
-h1.classList.toggle("teste1"); // verifica se TEM ele remove / se não TEM ele adicona
+h1.classList.toggle("teste1"); // verifica se TEM ele remove / se não TEM ele adicona.
 
 if (h1.classList.contains("teste1")) {
   // verificando se existe a class
