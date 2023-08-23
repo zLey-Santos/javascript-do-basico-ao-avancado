7ec3cf59cf319ff0560674e89667b68a3b50afae
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 21:28:47 +0100

    tabuada

--- 006-exercicios/002-tabuada/tabuada.js
+++ 006-exercicios/002-tabuada/tabuada.js
@@ -1,4 +1,4 @@
-const multiplicador = 9; // Valor a ser alterado
+const multiplicador = 9; // Valor a ser alterado.
 
 for (let valor = 0; valor <= 10; valor++) {
   const calculo = multiplicador * valor;
