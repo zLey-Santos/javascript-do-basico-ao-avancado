7b9a2cc73ce134a8138124a189bc8077c024ab32
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 21:19:11 +0100

    remainder

--- 003-operadores-aritimeticos/004-remainder/remainder.js
+++ 003-operadores-aritimeticos/004-remainder/remainder.js
@@ -6,4 +6,4 @@ console.log(5 % 2); //ele retorna 1 sendo assim o restante 1 a sobra / o resto
 // %(remainder ou resto ) serve para verificar os valores restante se ouve ou não dentro do calculo de divisão
 // podemos verificar no caso se o número é impár ou par se ele retorna 0 par se ele for maior que 0 retorna 1 impár
 
-// veremos sobre maior e menos mais a frente
+// veremos sobre maior e menos mais a frente..
