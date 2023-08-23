507b38f344a90e298c2954504a39e2513c32066c
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 21:15:17 +0100

    multiplicação

--- 003-operadores-aritimeticos/003-multiplicaçao-divisao/multiplicacaoDivisao.js
+++ 003-operadores-aritimeticos/003-multiplicaçao-divisao/multiplicacaoDivisao.js
@@ -1,7 +1,7 @@
 console.log(2 * 10); // independente da ordem o valor retorna sempre correto / retorna 20
 console.log(5 * 10); // nesse caso poderia ser 10 * 5 retona 50
 
-// já nas divisoẽs temos que ter mais atenção pois a ordem dos números altera o resultado
+// já nas divisoẽs temos que ter mais atenção pois a ordem dos números altera o resultado.
 console.log(50 / 10); // retona 5
 console.log(10 / 20); // rertorna 0.5
 console.log(20 / 10); // retorna 2
