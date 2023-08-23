a2d893d3e1568b527c27d0b292423a8afc68ce6e
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 21:48:37 +0100

    indice elementos

--- 009-arrays-a-fundo/002-indices-elementos/arr.js
+++ 009-arrays-a-fundo/002-indices-elementos/arr.js
@@ -7,4 +7,4 @@ const arr1 = [1, 2, 3, 4, true, "zLey", "Santos"];
 
 console.table(arr1); // Mostra uma tabela o index(indice) e value(valores)
 console.log(arr1[6]); // Pega a possição ou seja o indice do nosso array | para resgatar um possição usamos [colchetes]
-console.log(arr1.length); // Exibe o tamanho do meu array quantos valores tem dentro do array
+console.log(arr1.length); // Exibe o tamanho do meu array quantos valores tem dentro do array.
