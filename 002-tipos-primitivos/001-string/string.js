d001ffd5ea07133f34dbbc23bd8cce46aba13ff2
zLey-Santos <wy.santos92@gmail.com>
2023-08-23 21:11:23 +0100

    strings

--- 002-tipos-primitivos/001-string/string.js
+++ 002-tipos-primitivos/001-string/string.js
@@ -3,7 +3,7 @@ console.log("Olá, Mundo!");
 Como podemos escrever uma string
 'Com as pas simples'
 "Com aspas duplas"
-`Com crase (pode ser uma template lireal / literalmente pod ese passar tudo para ele)`
+`Com crase (pode ser uma template lireal / literalmente pod ese passar tudo para ele.)`
 */
 
 console.log(`Olá, 'Mundo!' "Nos estamos bem"`);
