826fcc274e9f43dae6efa4c7812a95c448a2c74b
zLey-Santos <wy.santos92@gmail.com>
2023-09-03 16:06:30 +0100

    Irmãos ( nextSibling, nextElementSibling )

---
+++ 013-encontrando-elementos/005-irmaos-nextSibling-nexElementSibling/dom.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector("h1");
+
+console.log(h1.nextSibling);
+console.log(h1.nextElementSibling);
+
+const p = document.querySelector(".paragrafo");
+
+console.log("1", p.nextSibling);
+console.log("2", p.nextElementSibling);
