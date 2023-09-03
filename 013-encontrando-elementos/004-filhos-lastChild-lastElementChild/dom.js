270ffbbd04f4005c38fe2df72bdaad64eb5cb5db
zLey-Santos <wy.santos92@gmail.com>
2023-09-03 13:26:52 +0100

    lastChild- lastElementChild

---
+++ 013-encontrando-elementos/004-filhos-lastChild-lastElementChild/dom.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+console.log(container.lastChild);
+console.log(container.lastElementChild);
+
+const p = document.querySelector(".paragrafo");
+
+console.log(p.lastChild);
+console.log(p.lastElementChild);
