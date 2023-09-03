a208adf5c1a4a5c915cd06f803504d4178eb130c
zLey-Santos <wy.santos92@gmail.com>
2023-09-03 13:12:26 +0100

    fristChild-fristElementchild

---
+++ 013-encontrando-elementos/003-filhos-firstChild- firstElementChild/dom.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//const container = document.querySelector(".container");
+
+//console.log(` meu filho: ${container.firstElementChild}`);
+
+const p = document.querySelector(".paragrafo");
+console.log(p.firstChild);
+console.log(p.firstElementChild);
