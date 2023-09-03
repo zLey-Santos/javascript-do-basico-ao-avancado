17dfbad59e1c0a660d07749fdce258d0bde8e6b0
zLey-Santos <wy.santos92@gmail.com>
2023-09-03 16:11:19 +0100

    Irmãos ( previousSibling, previousElementSibling

---
+++ 013-encontrando-elementos/006-irmaos-previousSibling-previousElementSibling/dom.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector("h1");
+
+console.log(h1.previousSibling);
+console.log(h1.previousElementSibling);
+
+const p = document.querySelector(".paragrafo");
+
+console.log("1", p.previousSibling);
+console.log("2", p.previousElementSibling);
